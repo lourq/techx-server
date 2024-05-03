5e8a1becbed4da99bcacea68265688e74bd60d39
lourq <lourzzz666@gmail.com>
2024-05-03 18:37:10 +0300

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = 80;
+const PORT = 443;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
