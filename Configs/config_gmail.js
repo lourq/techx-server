e44fc06d770d4eda884e5ff928da39a6c7ee01c7
HatoryHanzo <scrinnpowerapp@gmail.com>
2024-05-23 16:48:50 +0300

    updt

--- Configs/config_gmail.js
+++ Configs/config_gmail.js
@@ -9,32 +9,37 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
-const _postcard_path = path.join(
-  __dirname,
-  "../Content/html/GmailVerefication.html"
-);
+const _postcard_path = path.join(__dirname, "../Content/html/GmailVerefication.html");
 let _user_gmail;
 let _code;
 
-async function ReadHTMLFile() {
-  try {
+async function ReadHTMLFile() 
+{
+  try 
+  {
     const html = await fs.readFile(_postcard_path, "utf-8");
 
     return html;
-  } catch (error) {
+  } 
+  catch (error) 
+  {
     console.error("Error reading HTML file:", error);
     throw error;
   }
 }
 
-async function SendMail(postcard_content) {
-  try {
-    const transporter = nodemailer.createTransport({
+async function SendMail(postcard_content) 
+{
+  try 
+  {
+    const transporter = nodemailer.createTransport(
+    {
       service: "gmail",
       auth: { user: process.env.GMAIL, pass: process.env.PASSWORD_GMAIL },
     });
 
-    const mail_options = {
+    const mail_options = 
+    {
       from: process.env.GMAIL,
       to: _user_gmail,
       subject: "Confirm your email on TechX",
@@ -42,25 +47,26 @@ async function SendMail(postcard_content) {
     };
 
     await transporter.sendMail(mail_options);
-  } catch (error) {
-    console.error("Error sending email:", error);
   }
+  catch (error) { console.error("Error sending email:", error); }
 }
 
-function SEND_CODE_VERIFICATION(user_gmail, code) {
+function SEND_CODE_VERIFICATION(user_gmail, code) 
+{
   _user_gmail = user_gmail;
   _code = code;
 
-  (async () => {
-    try {
+  (async () => 
+  {
+    try 
+    {
       let postcard = await ReadHTMLFile();
 
       postcard = postcard.replace("<p>XXXX</p>", `<p>${_code}</p>`);
 
       await SendMail(postcard);
-    } catch (error) {
-      console.error("Error:", error);
-    }
+    } 
+    catch (error) { console.error("Error:", error); }
   })();
 }
 
