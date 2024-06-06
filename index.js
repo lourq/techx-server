3830b0260fb52d1205a5c1b2980feaa2e7f12800
HatoryHanzo <scrinnpowerapp@gmail.com>
2024-06-06 18:40:41 +0300

    descont_price for GettingDataForCarusel

--- index.js
+++ index.js
@@ -419,35 +419,35 @@ app.post("/GettingDataForCarusel", async (req, res) =>
   
     const formatted_data_iphones = iphones.filter(i => i.incarousel === true).map(i => 
     {
-        return { id: i.id, images: i.images[0], model: i.model, price: i.price };
+        return { id: i.id, images: i.images[0], model: i.model, price: i.price, descont_price: i.descont_price};
     });
 
     const airpods = await AirPodsModel.find();
 
     const formatted_data_airpods = airpods.filter(i => i.incarousel === true).map(i => 
     {
-      return { id: i.id, images: i.images[0], model: i.model, price: i.price };
+      return { id: i.id, images: i.images[0], model: i.model, price: i.price, descont_price: i.descont_price };
     });
 
     const applewatchs = await AppleWatchModel.find();
   
     const formatted_data_applewatchs = applewatchs.filter(i => i.incarousel === true).map(i => 
     {
-      return { id: i.id, images: i.images[0], model: i.model, price: i.price };
+      return { id: i.id, images: i.images[0], model: i.model, price: i.price, descont_price: i.descont_price };
     });
 
     const macbooks = await MacbookModel.find();
   
     const formatted_data_macbooks = macbooks.filter(i => i.incarousel === true).map(i => 
     {
-      return { id: i.id, images: i.images[0], model: i.model, price: i.price };
+      return { id: i.id, images: i.images[0], model: i.model, price: i.price, descont_price: i.descont_price };
     });
 
     const ipads = await IpadModel.find();
   
     const formatted_data_ipads = ipads.filter(i => i.incarousel === true).map(i => 
     {
-      return { id: i.id, images: i.images[0], model: i.model, price: i.price };
+      return { id: i.id, images: i.images[0], model: i.model, price: i.price, descont_price: i.descont_price };
     });
 
     const consoles = await ConsoleModel.find();
