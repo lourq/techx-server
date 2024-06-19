a974f9a18ad66cd38b4259a7022754506fd5d5e6
HatoryHanzo <scrinnpowerapp@gmail.com>
2024-06-20 00:43:39 +0300

    new version with CnangeProfilData

--- index.js
+++ index.js
@@ -287,6 +287,42 @@ app.post("/RemoveFromSession", async (req, res) =>
   }
 });
 //#endregion
+//#region [Profile сhange Requests.]
+      // Let's check if the user exists.
+app.post("/CnangeProfilData", async (req, res) => 
+{
+  try 
+  {
+    const token = req.token;
+    const { query } = req.body;
+    const session = await SessionModel.findOne({ token });
+    
+    if (!session)
+      return res.status(404).json({ message: "Session not found" });
+    
+    const user = await UserModel.findById(session.user_id);
+    
+    if (!user)
+      return res.status(404).json({ message: "User not found" });
+    
+    if (req.body.change_name)
+      user.name = req.body.change_name;
+    if (req.body.change_phone_number)
+      user.phone_number = req.body.change_phone_number;
+    if (req.body.change_delivery_address)
+      user.delivery_address = req.body.change_delivery_address;
+    
+    await user.save();
+    
+    res.status(200).json({ message: "Profile updated successfully", user });
+  } 
+  catch (error) 
+  {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+//#endregion
 //#region [For client Requests]
         //Search products on the site.
 app.post("/SearchForProducts", async (req, res) => 
