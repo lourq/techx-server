db0fc1b40c8e49406dd0d4b0244b92485f9605b3
lourq <lourzzz666@gmail.com>
2024-06-30 00:33:39 +0300

    update middleware

--- middlewares/corsMiddleware.js
+++ middlewares/corsMiddleware.js
@@ -1,15 +1,21 @@
-// Description: This file contains the middleware for CORS.
+// Description: This file contains the middleware for CORS and IP restriction.
 export const corsMiddleware = (req, res, next) => {
   const allowedOrigins = [
     "https://next-techx.vercel.app",
     "http://localhost:3000",
   ];
+  const allowedIPs = [
+    '76.76.21.98',
+    '76.76.21.22'
+  ];
   const origin = req.headers.origin;
+  const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-  if (allowedOrigins.includes(origin)) {
+  if (allowedOrigins.includes(origin) && allowedIPs.includes(clientIP)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   } else {
     res.setHeader("Access-Control-Allow-Origin", "null");
+    return res.status(403).json({ message: 'Forbidden' });
   }
 
   res.setHeader(
